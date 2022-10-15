b49217ad10b32b3ec2c474a95aea40112f0e400a
emanuelpedrozo <emanuelgpedrozo@gmail.com>
2022-10-15 16:46:50 -0300

    correcao tabela_unica

--- dags/trabalho2_dag_1.py
+++ dags/trabalho2_dag_1.py
@@ -66,10 +66,12 @@ def trabalho2_dag1():
         df2 = pd.read_csv(NOME_TABELA_3, sep=";")
         df3 = df.merge(df1, on=['Sex', 'Pclass'], how='inner')
         df4 = df2.merge(df3, on=['Sex', 'Pclass'], how='inner')
+        print(df4)
+        df4.to_csv(TABELA_UNICA, index=False, sep=";")
         return TABELA_UNICA
    
     inicio = DummyOperator(task_id="inicio")
-    #fim = DummyOperator(task_id="fim")
+    fim = DummyOperator(task_id="fim")
 
     @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
     def end():
@@ -84,7 +86,7 @@ def trabalho2_dag1():
     sibsp_parch = ind_sisb_parch(df)
     tab_final = get_together(indicador, ticket, sibsp_parch)
 
-    inicio >> df >> [indicador, ticket, sibsp_parch] >> tab_final >> triggerdag
+    inicio >> df >> [indicador, ticket, sibsp_parch] >> tab_final >> triggerdag >> fim
 
 execucao = trabalho2_dag1()
         
--- dags/trabalho2_dag_2.py
+++ dags/trabalho2_dag_2.py
@@ -16,7 +16,7 @@ def trabalho2_dag2():
     @task
     def media_total():
         NOME_TABELA = "/tmp/resultados.csv"
-        FILE = "/tmp/tabela_unica.csv"
+        file = r"/tmp/tabela_unica.csv"
         df = pd.read_csv(file, sep=";")
         res1 = df.groupby(['Sex'])['PassengerId', 'Fare', 'SibSp', 'Parch'].mean().reset_index()
         res1.to_csv(NOME_TABELA, index=False, sep=';')
